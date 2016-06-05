a99c160329a0d269e3f87944dd75b45bd65d2582
CaiQiqi <564933437@qq.com>
2016-06-05 16:44:54 +0800

    modified Constant.java

--- src/caiqiqi/sdnserver/Constants.java
+++ src/caiqiqi/sdnserver/Constants.java
@@ -2,7 +2,8 @@ package caiqiqi.sdnserver;
 
 public interface Constants {
 
-	public final static String RECEIVE_FILE_PATH = "C:\\GitProject\\SDNServer\\";
+	//public final static String RECEIVE_FILE_PATH = "C:\\GitProject\\SDNServer\\"; // for windows
+	public final static String RECEIVE_FILE_PATH = "/Users/caiqiqi/GitProjects/SDNServer";
 	
 	public final static int DEFAULT_BIND_PORT = 30000;
 	
